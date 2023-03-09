21e4e3c82e37f5755fa975b746771e4b3e493517
uomma <marcelloleone975@gmail.com>
2023-03-10 00:07:48 +0100

    mancava html script

--- SNACK-4  CALCIO/main.js
+++ SNACK-4  CALCIO/main.js
@@ -1,26 +1,11 @@
 'use strict'
-// nome, punti fatti, falli subiti
+
 
 const squadre = ['milan','juve','inter'];
-/*     {
-        nome : Milan,
-        puntiFatti: i,
-        falliSubiti : i 
-    },
-    {
-        nome : Juve,
-        puntiFatti: i,
-        falliSubiti : i 
-    },
-    {
-        nome : Inter,
-        puntiFatti: i,
-        falliSubiti : i 
-    }
-]; */
+
 
 console.log(squadre);
 const nuoviTeam = [];
-console.log(nuoviTeam)
+console.log(nuoviTeam   )
 
 
