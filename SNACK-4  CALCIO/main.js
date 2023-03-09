e7c2acd49b568539a9e24d14ed9aa80c0c524677
uomma <marcelloleone975@gmail.com>
2023-03-10 00:00:27 +0100

    array

---
+++ SNACK-4  CALCIO/main.js
@@ -0,0 +1,24 @@
+'use script'
+// nome, punti fatti, falli subiti
+
+const squadre =['milan','juve','inter'];
+/*     {
+        nome : Milan,
+        puntiFatti: i,
+        falliSubiti : i 
+    },
+    {
+        nome : Juve,
+        puntiFatti: i,
+        falliSubiti : i 
+    },
+    {
+        nome : Inter,
+        puntiFatti: i,
+        falliSubiti : i 
+    }
+]; */
+
+console.log(squadre);
+
+
